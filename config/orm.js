0e76fcbc42f564119dcae36d3a999fa24b92030a
bluecam74 <mabdulha@uci.edu>
2019-02-24 13:29:21 -0800

    set-up part 1

---
+++ config/orm.js
@@ -0,0 +1,30 @@
+var connection = require("./connection.js");
+ 
+var orm = {
+    selectAll: function() {
+        var queryString = "SELECT * FROM burgers";
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) throw err; 
+            console.log(result);
+        }); 
+    },
+    insertOne: function(x) {
+        var queryString = "INSERT INTO burgers burger_name, devoured VALUES ??, false";
+        console.log(queryString);
+        connection.query(queryString, [x], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    updateOne: function(x) {
+        var queryString = "UPDATE burgers SET devoured = true WHERE burger_name = ?";
+        console.log(queryString);
+        connection.query(queryString, [x], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    }
+}
+
+module.exports = orm; 
