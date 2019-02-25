912049ccd0fef363c4314c100f609d06a9bafba8
bluecam74 <mabdulha@uci.edu>
2019-02-24 18:33:05 -0800

    variable name changes

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,7 +3,7 @@ var burger = require("../models/burger");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-    burger.all(function(data) {
+    burger.selectAll(function(data) {
       var hbsObject = {
         burgers: data
       };
@@ -13,7 +13,7 @@ router.get("/", function(req, res) {
   });
   
   router.post("/api/burgers", function(req, res) {
-    burger.create([
+    burger.insertOne([
       "burger_name", "devoured"
     ], [
       req.body.burger_name, req.body.devoured
@@ -27,7 +27,7 @@ router.get("/", function(req, res) {
   
     console.log("condition", condition);
   
-    burger.update({
+    burger.updateOne({
       devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
@@ -38,17 +38,6 @@ router.get("/", function(req, res) {
     });
   });
   
-  router.delete("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    burger.delete(condition, function(result) {
-      if (result.affectedRows == 0) {
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
-  });
   
   module.exports = router;
   
--- server.js
+++ server.js
@@ -1,1 +1,36 @@
 var express = require("express");
+
+var PORT = process.env.PORT || 3306;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+var http=require('http');
+
+var server=http.createServer(function(req,res){
+    res.end('test');
+});
+
+server.on('listening',function(){
+    console.log('ok, server is running');
+});
+
+server.listen(80);
+
+
+app.listen(PORT, function() {
+  console.log("Server listening on:" + PORT);
+});
