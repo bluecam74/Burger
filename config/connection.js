eb66d6f1124e9d80d3c103c989dce6931b515d81
Malak Abdul <43797869+bluecam74@users.noreply.github.com>
2019-03-04 10:44:06 -0800

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: "127.0.0.1", 
     port: 8889, 
     user: "root", 
-    password: "root", 
+    password: "", 
     database: "burger_db", 
 });
 
